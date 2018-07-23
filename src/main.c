f4d4c5e5ac9fcf6411a2c5f7d213d0aa826c9804
Lucas Dumer <lucasdumer@gmail.com>
2018-07-23 01:01:14 -0300

    adicionado socket

--- src/main.c
+++ src/main.c
@@ -1,9 +1,38 @@
 #include <ncurses.h>
+#include <sys/socket.h> 
 
 int main(int argc, char* argv[])
 {
+    int ch;
+
     initscr();
-    getch();
+    cbreak();
+    noecho();
+    curs_set(0);
+    int socket_id = socket("127.0.0.1:22", SOCK_STREAM, 0);
+
+    printw(" AAA -> %d\n", socket_id);
+
+    while (true) {
+
+        ch = getch();
+        
+        switch (ch) {
+            case 65:
+                printw("UP\n");
+                break;
+            case 66:
+                printw("DOWN\n");
+                break;
+            case 67:
+                printw("RIGHT\n");
+                break;
+            case 68:
+                printw("LEFT\n");
+                break;
+        }
+    }
+
     endwin();
 
     return 0;
